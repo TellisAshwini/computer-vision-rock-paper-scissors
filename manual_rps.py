1dbb9d766a50c56c635700e60936c6e268479d6d
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-02-22 12:45:07 +0000

    Create 'play' function

--- manual_rps.py
+++ manual_rps.py
@@ -5,15 +5,11 @@ def get_computer_choice():
     computer_choice = random.choice(choice_list)
     return computer_choice
     
-
 def get_user_choice():
     user_choice = input("Please enter your choice here: ")
     return user_choice
 
-computer_choice = get_computer_choice()
-user_choice = get_user_choice()
-
-def get_winner(computer_choice, user_choice ):
+def get_winner(computer_choice, user_choice):
     #print(computer_choice, user_choice)
     if (computer_choice == 'rock' and user_choice == 'paper') or (computer_choice == 'paper' and user_choice == 'scissors')  or (computer_choice == 'scissors' and user_choice == 'rock'):
         print("You won!")
@@ -22,4 +18,10 @@ def get_winner(computer_choice, user_choice ):
     elif (user_choice == 'rock' and computer_choice == 'rock') or (user_choice == 'paper' and computer_choice == 'paper')  or (user_choice == 'scissors' and computer_choice == 'scissors'):
         print("It is a tie!")
 
-get_winner(computer_choice, user_choice)
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    winner = get_winner(computer_choice, user_choice)
+    return winner
+
+play()
