f8d8b641a830ff7a4bc704f352feb2846a5d5199
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-02-22 12:15:42 +0000

    Creates 'get_winner' function

--- manual_rps.py
+++ manual_rps.py
@@ -9,3 +9,17 @@ def get_computer_choice():
 def get_user_choice():
     user_choice = input("Please enter your choice here: ")
     return user_choice
+
+computer_choice = get_computer_choice()
+user_choice = get_user_choice()
+
+def get_winner(computer_choice, user_choice ):
+    print(computer_choice, user_choice)
+    if (computer_choice == 'Rock' and user_choice == 'Paper') or (computer_choice == 'Paper' and user_choice == 'Scissors')  or (computer_choice == 'Scissors' and user_choice == 'Rock'):
+        print("You won")
+    elif (user_choice == 'Rock' and computer_choice == 'Paper') or (user_choice == 'Paper' and computer_choice == 'Scissors')  or (user_choice == 'Scissors' and computer_choice == 'Rock'):
+        print("You lost")
+    elif (user_choice == 'Rock' and computer_choice == 'Rock') or (user_choice == 'Paper' and computer_choice == 'Paper')  or (user_choice == 'Scissors' and computer_choice == 'Scissors'):
+        print("It is a tie!")
+
+get_winner(computer_choice, user_choice)
