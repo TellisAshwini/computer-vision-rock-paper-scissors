974604273446b8992a92ac01541a8f3eaa0e2119
TellisAshwini <102413647+TellisAshwini@users.noreply.github.com>
2023-02-22 12:31:19 +0000

    Removes the print before the 'if statement' in get_winner function

--- manual_rps.py
+++ manual_rps.py
@@ -14,7 +14,7 @@ computer_choice = get_computer_choice()
 user_choice = get_user_choice()
 
 def get_winner(computer_choice, user_choice ):
-    print(computer_choice, user_choice)
+    #print(computer_choice, user_choice)
     if (computer_choice == 'rock' and user_choice == 'paper') or (computer_choice == 'paper' and user_choice == 'scissors')  or (computer_choice == 'scissors' and user_choice == 'rock'):
         print("You won")
     elif (user_choice == 'rock' and computer_choice == 'paper') or (user_choice == 'paper' and computer_choice == 'scissors')  or (user_choice == 'scissors' and computer_choice == 'rock'):
